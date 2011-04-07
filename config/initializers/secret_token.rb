1b2a1423c6c1fa02db8cd4e32a1060b2039d94bd
Jason Yanchuleff <jyanchuleff@gmail.com>
2011-04-07 15:24:31 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SummitConnect::Application.config.secret_token = '8fe46864b440fb78ed4bc74c793ad04d56eb3a554c11b8c9f87283f6aedac656a0d50e644d1ceda2f2a0da57c638352d256658e39d242753f5c5d7684fd49678'
